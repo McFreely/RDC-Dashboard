487cd495b4bb8dd785a230c13812de49e480efe0
McFreely <paulrmcfreely@gmail.com>
2013-03-22 22:40:42 +0100

    CODE: Mongoid layer added and functionnal; LAYOUT: display of movie and number of tweets added

--- dashboard.rb
+++ dashboard.rb
@@ -3,6 +3,17 @@ require 'sinatra'
 require 'slim'
 require 'multi_json'
 require 'httparty'
+require 'mongoid'
+
+Mongoid.load!("mongoid.yml")
+
+# DB = Mongo::Connection.db('MovieList', :pool_size => 5, :timeout => 5)
+
+class Movie
+	include Mongoid::Document
+	field :mt, as: :movie_title, type: String
+	field :tweets, type: Hash
+end
 
 class Twitter
 	include HTTParty
@@ -32,6 +43,8 @@ get '/' do
 end
 
 get '/manage' do
+	# @titles = Movie.find()
+	@movie = Movie.all
 	slim :manage
 end
 
@@ -40,11 +53,18 @@ post '/query/new' do
 	@tweet_list = []
 	@page = 1
 	twitter = Twitter.new
-	while @page  <= 1
-		response = twitter.search(@title, 1)
+	while @page <= 3
+		response = twitter.search(@title, @page)
 		response.each do |tweet|
-			@tweet_list <<  tweet['text']
+			@tweet_list << tweet['text']
 		end
+		@page += 1
+	end
+	movie = Movie.new(:mt => @title, :tweets => @tweet_list)
+	if movie.save
+		redirect '/manage'
+	else
+		"Error saving list"
 	end
 	slim :manage
 end
